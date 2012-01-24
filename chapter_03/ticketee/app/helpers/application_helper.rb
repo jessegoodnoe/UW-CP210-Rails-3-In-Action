16815e5b5ca0dd1581bb89b5c35bf9e372fed0fd
Jesse Goodnoe <jessegoodnoe@me.com>
2012-01-23 20:52:39 -0800

    Added title to show page.

--- chapter_03/ticketee/app/helpers/application_helper.rb
+++ chapter_03/ticketee/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def title(*parts)
+      unless parts.empty?
+        content_for :title do
+          (parts << "Ticketee").join(" - ") unless parts.empty?
+        end
+  end end
 end
