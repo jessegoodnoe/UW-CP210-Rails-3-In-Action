9f6f1f4ca738468fb6c6eaa4c71992f5e9c36f4c
Jesse Goodnoe <jessegoodnoe@me.com>
2012-02-05 22:47:26 -0800

    Lock down specifi controller actions for admins only.

--- ticketee/app/helpers/application_helper.rb
+++ ticketee/app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
   def title(*parts)
-      unless parts.empty?
-        content_for :title do
-          (parts << "Ticketee").join(" - ") unless parts.empty?
-        end
-  end end
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ") unless parts.empty?
+      end
+    end 
+  end
+  
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+    nil
+  end
 end
