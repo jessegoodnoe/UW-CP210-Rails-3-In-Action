32e99bfe00198eb84a6d8ea8957402900986d2be
Jesse Goodnoe <jessegoodnoe@me.com>
2012-02-13 20:32:35 -0800

    Restricting creating tickets to only users who have permissions to do it.

--- ticketee/features/step_definitions/permission_steps.rb
+++ ticketee/features/step_definitions/permission_steps.rb
@@ -1,5 +1,18 @@
-Given /^"([^"]*)" can view the "([^"]*)" project$/ do |user, project|
-  Permission.create!(:user => User.find_by_email!(user),
-                     :thing => Project.find_by_name!(project),
-                     :action => "view")
+# Given "user" can [create/view project/tickets] on/in the "blah" project
+#Given "user@ticketee.com" can create tickets on the "Ticketee" project
+#Given "user@ticketee.com" can view the "Ticketee" project
+#Given "user@ticketee.com" can edit tickets in the "Ticketee" project
+permission_step = /^"([^"]*)" can ([^"]*?) ([o|i]n)?\s?the "([^"]*)" project$/
+
+Given permission_step do |user, permission, on, project|
+  create_permission(user, find_project(project), permission)
+end
+
+def create_permission(email, object, action)
+  Permission.create!(:user => User.find_by_email!(email),
+                     :thing => object,
+                     :action => action)
+end
+def find_project(name)
+  Project.find_by_name!(name)
 end
