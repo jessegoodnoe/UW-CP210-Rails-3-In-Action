2e0d3dbe3e208fc6e0fe6e2d2f5ee444a0226fd4
Jesse Goodnoe <jessegoodnoe@me.com>
2012-02-19 20:55:58 -0800

    When updating a comment's status, it also updates the ticket's status

--- ticketee/app/models/comment.rb
+++ ticketee/app/models/comment.rb
@@ -1,5 +1,17 @@
 class Comment < ActiveRecord::Base
+  after_create :set_ticket_state
+
+  belongs_to :ticket
+  belongs_to :user
+  belongs_to :state
+
+  delegate :project, :to => :ticket
+  
   validates :text, :presence => true
   
-  belongs_to :user
+  private
+    def set_ticket_state
+      self.ticket.state = self.state
+      self.ticket.save!
+    end
 end
--- ticketee/app/models/ticket.rb
+++ ticketee/app/models/ticket.rb
@@ -1,11 +1,13 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  belongs_to :state
+  
   validates :title, :presence => true
-  validates :description, :presence => true,
-                          :length => { :minimum => 10 }
+  validates :description, :presence => true, :length => { :minimum => 10 }
 
   has_many :assets
   accepts_nested_attributes_for :assets
+  
   has_many :comments
 end
